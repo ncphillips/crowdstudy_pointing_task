70e04f553b17bc6aad19e917f8be69cb2866db5f
Nolan Phillips <ncphillips.19@gmail.com>
2015-07-20 16:30:10 +0000

    Stats

---
+++ public/scripts/Stats.jsx
@@ -0,0 +1,121 @@
+'use strict';
+
+//var React = require('react');
+var TOOLTIPS = {
+  row: {
+    worker_last_block: "Your Last Round",
+    worker_average_block: "Your Avg. Round",
+    population_average_block: "Avg. Worker's Round",
+    population_elite_block: "Top Worker's Round"
+  },
+  col: {
+    time: "Time",
+    time_per_target: "Time/Target",
+    num_misses: "# Misses",
+    misses_per_target: "Misses/Target"
+  }
+};
+
+var LABELS = {
+  row: {
+    worker_last_block: "Last Round",
+    worker_average_block: "Avg. Round",
+    population_average_block: "Avg. Worker's Round",
+    population_elite_block: "Top Worker's Round"
+  },
+  col: {
+    time: "Time",
+    time_per_target: "Time/Target",
+    num_misses: "# Misses",
+    misses_per_target: "Misses/Target"
+  }
+};
+
+if (typeof require !== 'undefined') {
+  var Questions = require('./Questions');
+}
+
+
+var StatsRow = React.createClass({
+  render: function () {
+    return (
+      <tr>
+        <td>{this.props.name}</td>
+        <td>{Math.round(this.props.data.time / 100) / 10} sec.</td>
+        <td>{Math.round(this.props.data.time_per_target)/1000 } sec.</td>
+        <td>{Math.round(this.props.data.num_misses * 100)/100}</td>
+        <td>{Math.round(this.props.data.misses_per_target * 100)/100}</td>
+      </tr>
+    );
+  },
+  getDefaultProps: function () {
+    return {
+      className: '',
+      colorCells: false
+    };
+  }
+});
+
+
+var StatsView = React.createClass({
+  render: function () {
+    var rows = [];
+    if (this.state.stats.worker.last_block) {
+      rows.push(<StatsRow name={LABELS.row.worker_last_block} data={this.state.stats.worker.last_block}/>);
+    }
+    if (this.state.stats.worker.average) {
+      rows.push(<StatsRow name={LABELS.row.worker_average_block} data={this.state.stats.worker.average}/>);
+    }
+    if (this.state.stats.population.average) {
+      rows.push(<StatsRow name={LABELS.row.population_average_block} data={this.state.stats.population.average}/>);
+    }
+    if (this.state.stats.population.elite) {
+      rows.push(<StatsRow name={LABELS.row.population_elite_block} data={this.state.stats.population.elite}/>);
+    }
+    return (
+      <div>
+        <h2>Feedback</h2>
+        <table className="table">
+          <thead>
+            <tr>
+              <td></td>
+              <td><span data-toggle="tooltip" data-placement="top" title={TOOLTIPS.col.time}>{LABELS.col.time}</span></td>
+              <td><span data-toggle="tooltip" data-placement="top" title={TOOLTIPS.col.time_per_target}>{LABELS.col.time_per_target}</span></td>
+              <td><span data-toggle="tooltip" data-placement="top" title={TOOLTIPS.col.num_misses}>{LABELS.col.num_misses}</span></td>
+              <td><span data-toggle="tooltip" data-placement="top" title={TOOLTIPS.col.misses_per_target}>{LABELS.col.misses_per_target}</span></td>
+            </tr>
+          </thead>
+          <tbody>
+          {rows}
+          </tbody>
+          <tfoot>
+          </tfoot>
+        </table>
+        <Questions callback={this.props.callback}/>
+      </div>
+    )
+  },
+  componentDidMount: function () {
+    $.ajax({
+      type: 'GET',
+      url: '/pointing_task/' + this.props.worker._id + '/stats',
+      dataType: 'json',
+      success: this.setComparisonStats,
+      error: function (a, b, c) {
+        console.log(a, b, c);
+      }
+    });
+    $('[data-toggle="tooltip"]').tooltip();
+  },
+  getInitialState: function () {
+    return { stats: {worker: {}, population: {} } };
+  },
+  setComparisonStats: function (stats) {
+    this.setState({stats: stats});
+  }
+
+});
+
+if (typeof module !== 'undefined') {
+  module.exports = StatsView;
+}
---
+++ public/scripts/lib/pointingstats.js
@@ -0,0 +1,172 @@
+var statsandstones = (function () {
+  "use strict";
+
+  /**
+   * This function aggregates a homogenous array of stats objects
+   * by finding the mean value for each stats value.
+   *
+   * Each stats object must be a non-nested object with a Number value (int or float).
+   *
+   *
+   * @param stats
+   */
+  function aggregateStats(stats) {
+    var result = {};
+
+    stats.forEach(function (stat) {
+      for (var name in stat) {
+        if (stat.hasOwnProperty(name)) {
+          if (!result.hasOwnProperty(name)){
+            result[name] = 0;
+          }
+          result[name] += parseInt(stat[name]) || 0;
+        }
+      }
+    });
+
+    for (var name in result) {
+      if (result.hasOwnProperty(name)) {
+        result[name] = result[name] / stats.length;
+      }
+    }
+    return result;
+  }
+
+  /**
+   * Sorts stats objects using Quick sort.
+   *
+   * @param stats
+   * @param property_name
+   * @param reverse
+   * @returns {*}
+   */
+  function sortStats(stats, property_name, reverse) {
+    if (typeof reverse === 'undefined') {
+      reverse = false;
+    }
+    // Order these by the property_name;
+    if (stats.length <= 1) {
+      return stats;
+    }
+
+    var pivot = Math.floor((stats.length - 1) / 2);
+    var val = stats[pivot];
+
+    var less = [];
+    var more = [];
+
+    var stat;
+    for (var i=0; i < pivot; i++) {
+      stat = stats[i];
+      if (reverse) {
+        stat[property_name] < val[property_name] ? less.push(stat): more.push(stat);
+      } else {
+        stat[property_name] > val[property_name] ? less.push(stat): more.push(stat);
+      }
+    }
+
+    for (i=pivot+1; i < stats.length; i++) {
+      stat = stats[i];
+      if (reverse) {
+        stat[property_name] < val[property_name] ? less.push(stat): more.push(stat);
+      } else {
+        stat[property_name] > val[property_name] ? less.push(stat): more.push(stat);
+      }
+    }
+
+    return (sortStats(less, property_name, reverse)).concat([val], sortStats(more, property_name, reverse));
+  }
+
+  return {
+    aggregateStats: aggregateStats,
+    sortStats: sortStats
+  };
+
+})();
+
+var pointingstats = (function () {
+  "use strict";
+
+  /**
+   * Generates a stats object for a single Whack a Mole block.
+   *
+   * @param block
+   * @returns {{time: number, time_per_mole: number, num_misses: number, misses_per_mole: number, num_hits: number, score: number}}
+   */
+  function generateBlockStats(block) {
+    var stats = {
+      time: 0,
+      num_misses: 0,
+      time_per_target: 0,
+      misses_per_target: 0
+    };
+    var log;
+    if (block && block.click_logs) {
+      for (var i=0; i < block.click_logs.length; i++) {
+        log = block.click_logs[i];
+        stats.time += parseInt(log.end_time) - parseInt(log.start_time);
+        stats.num_misses += parseInt(log.errors);
+      }
+
+      stats.num_targets = block.click_logs.length;
+      stats.time_per_target = stats.time / block.click_logs.length;
+      stats.misses_per_target = stats.num_misses / block.click_logs.length;
+    }
+
+    return stats;
+  }
+
+
+  /**
+   * Generates a single stats object, given an array of blocks.
+   *
+   * @param blocks
+   * @returns {*}
+   */
+  function generateAverageBlockStats(blocks) {
+    // Generate the stats object for every block.
+    var block_stats = blocks.map(generateBlockStats);
+
+    // Aggregate all block stats objects and return the result;
+    return statsandstones.aggregateStats(block_stats);
+  }
+
+  function generatePopulationEliteStats(workers) {
+    // Get the aggregated stats object for  a worker.
+    var n = Math.ceil(workers.length * 0.15) || 1;
+    var worker_average_stats = workers.map(function (worker) {
+      return generateAverageBlockStats(worker.experiments.pointing_task.data);
+    });
+    var fastest_workers = statsandstones.sortStats(worker_average_stats, 'time', true);
+    var scoriest_workers = statsandstones.sortStats(worker_average_stats, 'num_misses', true);
+    var elite_worker_stats = fastest_workers.splice(0, n).concat(scoriest_workers.splice(0, n));
+    return statsandstones.aggregateStats(elite_worker_stats);
+  }
+
+  function generatePopulationAverageStats(workers) {
+    var worker_average_stats = workers.map(function (worker) {
+      if (worker.experiments &&
+        worker.experiments.pointing_task &&
+        worker.experiments.pointing_task.data
+      ){
+
+        return generateAverageBlockStats(worker.experiments.pointing_task.data);
+      }
+    });
+
+    return statsandstones.aggregateStats(worker_average_stats);
+
+  }
+
+  return {
+    generateBlockStats: generateBlockStats,
+    generateAverageStats: generateAverageBlockStats,
+    generatePopulationAverageStats: generatePopulationAverageStats,
+    generatePopulationEliteStats: generatePopulationEliteStats
+  };
+})();
+
+if (typeof module !== 'undefined') {
+  module.exports.pointingstats = pointingstats;
+  module.exports.statsandstones = statsandstones;
+}
