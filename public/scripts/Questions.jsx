aa0e348a3540735782b03ede20014f18259567b7
ncphillips <ncphillips@upei.ca>
2015-08-13 17:07:04 -0300

    Changed to 'comparison' and 'no_comparison' questions. If experiment feedback_type is None then always show 'no_comparison'

--- public/scripts/Questions.jsx
+++ public/scripts/Questions.jsx
@@ -1,11 +1,11 @@
 var _questions = {
-  first_feedback_questions: [
+  no_comparison: [
     "Last round, how many times did you miss?",
     "Last round, what was your time/target?",
     "Last round, do you feel like you performed well?",
     "Last round, did you try hard?"
   ],
-  other_feedback_questions: [
+  comparison: [
     "Last round, were you faster than the Average worker?",
     "Last round, were you faster than the Expert worker?",
     "Last round, did you miss fewer times than the Average worker?",
